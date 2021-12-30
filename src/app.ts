25d7e6ed3946a4563ac8ca74fa9f123700168d65
alstn <tkops365@gmail.com>
2021-12-30 21:56:42 +0900

    백엔드 ㅛㄷㅅㅅ

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createServer, Server } from 'http';
-
+import bodyParser from 'body-parser';
 import controller from './controller';
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(controller);
 
 const server = createServer(app);
